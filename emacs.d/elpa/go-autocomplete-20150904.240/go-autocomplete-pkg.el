b6d3ad515472e3e141a6ed90ab1318489262aeb2
YunTaek Yi <ytyi@cognet9.com>
2021-05-24 08:14:53 +0900

    modify emacs module

--- emacs.d/elpa/go-autocomplete-20150904.240/go-autocomplete-pkg.el
+++ emacs.d/elpa/go-autocomplete-20150904.240/go-autocomplete-pkg.el
@@ -1,2 +1,2 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "go-autocomplete" "20150904.240" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")) :commit "939b4a677f2f843ea13d9dd90206d57111f0ceb9" :keywords '("languages") :authors '(("Mikhail" . "tensai@cirno.in")) :maintainer '("Mikhail" . "tensai@cirno.in"))
+;;; Generated package description from /Users/mirwing/.emacs.d/elpa/go-autocomplete-20150904.240/go-autocomplete.el  -*- no-byte-compile: t -*-
+(define-package "go-autocomplete" "20150904.240" "auto-complete-mode backend for go-mode" '((auto-complete "1.4.0")) :commit "4acdcbdea79de6b3dee1c637eca5cbea0fdbe37c" :keywords '("languages") :authors '(("Mikhail" . "tensai@cirno.in")) :maintainer '("Mikhail" . "tensai@cirno.in"))
---
+++ emacs.d/elpa/lsp-dart-20210506.34/lsp-dart-pkg.el
@@ -0,0 +1,15 @@
+(define-package "lsp-dart" "20210506.34" "Dart support lsp-mode"
+  '((emacs "26.1")
+    (lsp-treemacs "0.3")
+    (lsp-mode "7.0.1")
+    (dap-mode "0.6")
+    (f "0.20.0")
+    (dash "2.14.1")
+    (pkg-info "0.4")
+    (dart-mode "1.0.5"))
+  :commit "96949d1c1cb5c63eb17ebbd43a125abead79149f" :keywords
+  '("languages" "extensions")
+  :url "https://emacs-lsp.github.io/lsp-dart")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
