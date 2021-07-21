6fbf01bfd8756ed55deb0402961c0d5928534b56
mirwing <mirwing@mirwing.com>
2021-07-21 09:21:23 +0900

    add init-zsh

---
+++ init.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/ohmyzsh/ohmyzsh.git oh-my-zsh
+git clone https://github.com/romkatv/powerlevel10k.git oh-my-zsh/custom/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions.git oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/custom/plugins/zsh-syntax-highlighting
