40ee80a56a5e3a44b5197ce7a1edba6b9933a74b
mirwing <mirwing@mirwing.com>
2022-01-20 15:57:51 +0900

    update shell script

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ git clone https://github.com/ohmyzsh/ohmyzsh.git oh-my-zsh
 git clone https://github.com/romkatv/powerlevel10k.git oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions.git oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/VundleVim/Vundle.vim.git vim/bundle/Vundle.vim
